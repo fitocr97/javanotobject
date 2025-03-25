26bf69906bc02323499c86af66c4c01e02ac44d6
Rodolfo Mora <rodolfomora97@outlook.com>
2025-03-25 13:43:26 -0600

    initial commit

---
+++ src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author Rodolfo
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        NewJFrame form = new NewJFrame();
+        form.setVisible(true);
+    }
+}
